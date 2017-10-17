9ff93f71349da8b317570326fdc5ce95ee8cf075
Jenna Hight <jennamhight@gmail.com>
2017-10-17 15:11:59 -0400

    Add model tests for Users and Mountains

--- app/models/mountain.rb
+++ app/models/mountain.rb
@@ -1,13 +1,65 @@
 class Mountain < ApplicationRecord
+  STATES = [
+	        'Alabama',
+	        'Alaska',
+	        'Arizona',
+          'Arkansas',
+	        'California',
+	        'Colorado',
+	        'Connecticut',
+	        'Delaware',
+	        'Florida',
+	        'Georgia',
+	        'Hawaii',
+	        'Idaho',
+	        'Illinois',
+	        'Indiana',
+	        'Iowa',
+	        'Kansas',
+	        'Kentucky',
+	        'Louisiana',
+	        'Maine',
+	        'Maryland',
+	        'Massachusetts',
+	        'Michigan',
+	        'Minnesota',
+	        'Mississippi',
+	        'Missouri',
+	        'Montana',
+	        'Nebraska',
+	        'Nevada',
+	        'New Hampshire',
+	        'New Jersey',
+	        'New Mexico',
+	        'New York',
+	        'North Carolina',
+	        'North Dakota',
+	        'Ohio',
+	        'Oklahoma',
+	        'Oregon',
+	        'Pennsylvania',
+	        'Rhode Island',
+	        'South Carolina',
+	        'South Dakota',
+	        'Tennessee',
+	        'Texas',
+	        'Utah',
+	        'Vermont',
+	        'Virginia',
+	        'Washington',
+	        'West Virginia',
+	        'Wisconsin',
+	        'Wyoming' ]
+
   belongs_to :creator,
     class_name: 'User',
     foreign_key: "creator_id"
 
-  validates :username, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true
   validates :city, presence: true
   validates :address, presence: true
-  validates :state, presence: true
-  validates :zip, presence: true
+  validates :state, presence: true, inclusion: { in: STATES }
+  validates :zip, presence: true, length: { is: 5}
 
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_valid(:username).when("username1")}
+  it { should_not have_valid(:username).when("", "12345", "textlongerthantwentycharacters")}
+
+  it { should have_valid(:email).when("johnsmith@gmail.com")}
+  it { should_not have_valid(:email).when("", "johnsmith.com")}
 end
