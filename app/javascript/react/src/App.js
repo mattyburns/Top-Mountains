14f8749380be5444c783470ab8a56f7cc04c068f
skboyle <boyle.steven@gmail.com>
2017-10-19 11:38:11 -0400

    Fixing config issue with webpack

--- app/javascript/react/src/App.js
+++ app/javascript/react/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
-const App = props => {
-  return(
-    <h1>Hello World</h1>
-  )
-}
+class App extends React.Component{
+ constructor(props) {
+   super(props);
+   this.state ={
 
+   }
+ }
+  render() {
+
+
+    return(
+      <h1>Hello World</h1>
+    )
+  }
+}
 export default App
