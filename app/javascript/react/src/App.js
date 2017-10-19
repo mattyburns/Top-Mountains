7c1db63a7648a9d4ba63d3170ca77ff7ee0b5e27
Matty Burns <matty@Mattys-MacBook-Air.local>
2017-10-19 14:04:43 -0400

    Merging with local master

--- app/javascript/react/src/App.js
+++ app/javascript/react/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import AppContainer from './AppContainer'
 
-const App = props => {
-  return(
-    <h1>Hello World</h1>
-  )
+class App extends React.Component{
+ constructor(props) {
+   super(props);
+ }
+  render() {
+
+    return(
+      <div>
+      <h1>App</h1>
+        <AppContainer />
+      </div>
+    )
+  }
 }
-
 export default App
---
+++ app/javascript/react/src/ReviewFormContainer.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react'
+import FormItem from './FormItem'
+
+
+class ReviewFormContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rating: '',
+      skiingReview: '',
+      foodReview: '',
+      lodgingReview: ''
+    }
+    this.handleRating = this.handleRating.bind(this);
+    this.handleSkiingReview = this.handleSkiingReview.bind(this);
+    this.handleFoodReview = this.handleFoodReview.bind(this);
+    this.handleLodgingReview = this.handleLodgingReview.bind(this);
+    this.handleSubmitForm = this.handleSubmitForm.bind(this);
+  }
+
+  handleRating(event) {
+    this.setState({ rating: event.target.value })
+    }
+
+  handleSkiingReview(event) {
+    this.setState({ skiingReview: event.target.value })
+    }
+
+  handleFoodReview(event) {
+    this.setState({ foodReview: event.target.value })
+    }
+
+  handleLodgingReview(event) {
+    this.setState({ lodgingReview: event.target.value })
+    }
+
+  handleSubmitForm(event){
+    console.log(this.state)
+    event.preventDefault();
+    let mountainPayload = {
+      rating: this.state.rating,
+      skiingReview: this.state.skiingReview,
+      foodReview: this.state.foodReview,
+      lodgingReview: this.state.lodgingReview,
+    }
+    console.log(mountainPayload)
+  }
+
+
+  render() {
+    return (
+      <form className="callout" id="shipping-address-form">
+        <h4>Review Form</h4>
+
+        <FormItem
+          name="rating"
+          content={this.state.rating}
+          nameText="Rating:"
+          handler={this.handleRating}
+        />
+
+        <FormItem
+          name="skiingReview"
+          content={this.state.skiingReview}
+          nameText="Skiing Review:"
+          handler={this.handleSkiingReview
+          }
+        />
+
+        <FormItem
+          name="foodReview"
+          content={this.state.foodReview}
+          nameText="Food Review:"
+          handler={this.handleFoodReview}
+        />
+
+        <FormItem
+          name="lodgingReview"
+          content={this.state.lodgingReview}
+          nameText="Lodging Review:"
+          handler={this.handleLodgingReview}
+        />
+
+        <input type="submit" className="button" value="Submit " onClick={this.handleSubmitForm} />
+      </form>
+    )
+  }
+}
+
+export default ReviewFormContainer
