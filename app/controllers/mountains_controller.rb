083901bacad12d28129b91434e6f2160e23f2b09
skboyle <boyle.steven@gmail.com>
2017-10-27 10:14:07 -0400

    Css improvements

--- app/controllers/mountains_controller.rb
+++ app/controllers/mountains_controller.rb
@@ -1,6 +1,11 @@
 class MountainsController < ApplicationController
 
   def index
-    @mountains = Mountain.search(params[:search])
+    # @mountains = Mountain.where("name ILIKE ?", params[:search])
+    @search = params[:search]
+    @mountains = Mountain.search(@search)
+    # if !@mountains
+    #   # redirect_to root_path
+    # end
   end
 end
